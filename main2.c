2d644309ac572cacd711a653a8cd0e2ddba1351c
StardustPhoton <kenschirodaviawamura@gmail.com>
2025-01-09 10:18:10 -0300

    Alterando a mensagem do texto

--- main2.c
+++ main2.c
@@ -4,7 +4,7 @@ int main() {
     char resposta;
     
     do {
-        printf("Bem-vindo ao nosso mundo!\n");
+        printf("Seja Bem-vindo ao nosso mundo!\n");
         printf("Deseja continuar (s/n)? ");
         scanf(" %c", &resposta);  // O espaço antes do %c é para ignorar qualquer caractere de nova linha residual.
 
